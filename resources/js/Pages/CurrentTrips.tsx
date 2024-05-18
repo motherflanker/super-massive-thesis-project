24026e65653e7989336db549787656b6a7295068
motherflanker <geth.jhjgh@mail.ru>
2024-05-18 19:51:13 +0300

    CurrentTrips filters

--- resources/js/Pages/CurrentTrips.tsx
+++ resources/js/Pages/CurrentTrips.tsx
@@ -1,13 +1,15 @@
 import IPaginateTrip from "@/types/models/IPaginateBooking"
-import React, { useEffect } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import { route } from "ziggy-js"
-import { Col, Divider, Space, Table, TablePaginationConfig, Popconfirm, Button, Tooltip } from "antd"
+import { Col, Divider, Space, Table, TablePaginationConfig, Popconfirm, Button, Tooltip, InputRef, Input } from "antd"
 import { Inertia, Method } from "@inertiajs/inertia"
 import { InertiaLink } from "@inertiajs/inertia-react"
-import { DeleteOutlined, EditOutlined, PlusCircleOutlined } from '@ant-design/icons'
+import { SearchOutlined } from '@ant-design/icons'
 import Template from "@/Components/Template"
 import IPaginateTravel from "@/types/models/IPaginateTravel"
 import IBus from "@/types/IBus"
+import { ColumnType, FilterDropdownProps } from "antd/es/table/interface"
+import Highlighter from "react-highlight-words"
 
 
 
@@ -24,20 +26,158 @@ type Props = TravelProps & BusProps
 
 
 const CurrentTrips: React.FC<Props> = ({ travels, buses }) => {
+
+  const [searchText, setSearchText] = useState('');
+  const [searchedColumn, setSearchedColumn] = useState('');
+  const searchInput = useRef<InputRef>(null);
+
+  const handleSearch = (
+    selectedKeys: string[],
+    confirm: FilterDropdownProps['confirm'],
+    dataIndex: any,
+  ) => {debugger
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+
+  const handleReset = (clearFilters: () => void) => {
+    clearFilters();
+    setSearchText('');
+  };
+
+
+  const getColumnSearchProps = (dataIndex: any): ColumnType<any> => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+      <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+          style={{ marginBottom: 8, display: 'block' }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Найти
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Сбросить
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({ closeDropdown: false });
+              setSearchText((selectedKeys as string[])[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            Назад
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered: boolean) => (
+      <SearchOutlined style={{ color: filtered ? '#1677ff' : undefined }} />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex]
+        .toString()
+        .toLowerCase()
+        .includes((value as string).toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+      ) : (
+        text
+      ),
+  });
+
   const TripTableColumns = [
-    { title: 'ID', dataIndex: 'travel_id', key: 'travel_id' },
-    { title: 'Номер рейса', dataIndex: 'tripNumber', key: 'tripNumber' },
-    { title: 'Откуда', dataIndex: 'origin', key: 'origin' },
-    { title: 'Куда', dataIndex: 'destination', key: 'destination' },
+    { title: 'ID', dataIndex: 'travel_id', key: 'travel_id', sorter: (a: any, b: any) => a.travel_id - b.travel_id },
+    {
+      title: 'Номер рейса', dataIndex: 'tripNumber', key: 'tripNumber', sorter: (a: any, b: any) => a.tripNumber - b.tripNumber,
+      ...getColumnSearchProps('tripNumber'),
+      onFilter: (value: any, record: any) => record.tripNumber.indexOf(value as string) === 0,
+    },
+    {
+      title: 'Откуда', dataIndex: 'origin', key: 'origin',
+      ...getColumnSearchProps('origin'),
+      onFilter: (value: any, record: any) => record.origin.indexOf(value as string) === 0,
+      sorter: (a: any, b: any) => a.origin.localeCompare(b.origin)
+    },
+    {
+      title: 'Куда', dataIndex: 'destination', key: 'destination',
+      ...getColumnSearchProps('destination'),
+      onFilter: (value: any, record: any) => record.destination.indexOf(value as string) === 0,
+      sorter: (a: any, b: any) => a.destination.localeCompare(b.destination)
+    },
     { title: 'Имя водителя', dataIndex: 'name', key: 'name' },
     { title: 'Фамилия водителя', dataIndex: 'surname', key: 'surname' },
     { title: 'Тел.номер водителя', dataIndex: 'phone', key: 'phone' },
     { title: 'Номер автобуса', dataIndex: 'plate_number', key: 'plate_number' },
-    { title: 'Места', dataIndex: 'max_seats', key: 'max_seats' },
-    { title: 'Статус', dataIndex: 'status', key: 'status' },
-    { title: 'Тип поездки', dataIndex: 'type', key: 'type' },
-    { title: 'Время отправления', dataIndex: 'departure_DateTime', key: 'departure_DateTime' },
-    { title: 'Время прибытия', dataIndex: 'arrival_DateTime', key: 'arrival_DateTime' },
+    {
+      title: 'Статус', dataIndex: 'status', key: 'status', filters: [
+        {
+          text: 'ожидает',
+          value: 'ожидает',
+        },
+        {
+          text: 'в пути',
+          value: 'в пути',
+        },
+        {
+          text: 'в пути обратно',
+          value: 'в пути обратно',
+        },
+      ],
+      onFilter: (value: any, record: any) => record.status.indexOf(value as string) === 0,
+    },
+    {
+      title: 'Тип поездки', dataIndex: 'type', key: 'type', filters: [
+        {
+          text: 'круговой рейс',
+          value: 'круговой рейс',
+        },
+        {
+          text: 'в один конец',
+          value: 'в один конец',
+        },
+      ],
+      onFilter: (value: any, record: any) => record.type.indexOf(value as string) === 0,
+    },
+    {
+      title: 'Время отправления', dataIndex: 'departure_DateTime', key: 'departure_DateTime',
+      onFilter: (value: any, record: any) => record.departure_DateTime.indexOf(value as string) === 0,
+      sorter: (a: any, b: any) => a.departure_DateTime.localeCompare(b.departure_DateTime)
+    },
+    {
+      title: 'Время прибытия', dataIndex: 'arrival_DateTime', key: 'arrival_DateTime',
+      onFilter: (value: any, record: any) => record.arrival_DateTime.indexOf(value as string) === 0,
+      sorter: (a: any, b: any) => a.arrival_DateTime.localeCompare(b.arrival_DateTime)
+    },
   ]
 
   const handleTableDataChange = (
@@ -45,8 +185,11 @@ const CurrentTrips: React.FC<Props> = ({ travels, buses }) => {
     filters: unknown,
     sorter: unknown
   ) => {
-    const url = route('trips.list') + `?page=${pagination.current}`
-    Inertia.visit(url, { method: Method.GET })
+    if (pagination.current !== travels.current_page) {
+      debugger
+      const url = route('currenttrips.list') + `?page=${pagination.current}`
+      Inertia.visit(url, { method: Method.GET })
+    }
   }
 
   useEffect(() => {
@@ -83,7 +226,7 @@ const CurrentTrips: React.FC<Props> = ({ travels, buses }) => {
   return (
     <Template>
       <div className="site-layout-background" style={{ padding: 14, minHeight: 360 }}>
-        <div style={{marginTop: -20}}>
+        <div style={{ marginTop: -20 }}>
           <Divider orientation="left">
             Карта
           </Divider>
@@ -99,6 +242,7 @@ const CurrentTrips: React.FC<Props> = ({ travels, buses }) => {
             rowKey={'trip_id'}
             dataSource={travels.data}
             columns={TripTableColumns}
+            size="middle"
             onChange={handleTableDataChange}
             pagination={{
               current: travels.current_page,
